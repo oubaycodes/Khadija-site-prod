944034884b1554dee3827ffcadecd65dde31cfcf
Oubay Chetouani <oubaycodes@gmail.com>
2024-09-10 00:45:30 +0100

    prod scares me

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 /* eslint-disable no-console */
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const fs = require("fs");
 const app = require("./app");
-
-dotenv.config({ path: "./config.env" });
-const port = +process.env.PORT;
+let port;
+if (process.env.NODE_ENV !== "prod") {
+  const dotenv = require("dotenv");
+  dotenv.config({ path: "./config.env" });
+  port = +process.env.PORT;
+} else if (process.env.NODE_ENV === "prod") {
+  fs.readdir("/etc/secrets").then((data) => {
+    console.log(data);
+  });
+}
 
 app.listen(port, async () => {
   console.log(`Listening on port ${port}.....`);
+
   const DB = process.env.DB_URI.replace("<password>", process.env.DB_PASSWORD);
   mongoose.set("strictQuery", true);
   await mongoose.connect(DB);
