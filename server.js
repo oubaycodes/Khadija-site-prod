f227ff0a031a50eb1d2cb41b2a7a70a825ef3114
Oubay Chetouani <oubaycodes@gmail.com>
2024-09-10 00:54:14 +0100

    more env magic

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 /* eslint-disable no-console */
 const mongoose = require("mongoose");
-const fs = require("fs");
 const app = require("./app");
-let port;
+
 if (process.env.NODE_ENV !== "prod") {
   const dotenv = require("dotenv");
   dotenv.config({ path: "./config.env" });
-  port = +process.env.PORT;
-} else if (process.env.NODE_ENV === "prod") {
-  fs.readdir("/etc/secrets").then((data) => {
-    console.log(data);
-  });
 }
+const port = +process.env.PORT;
 
 app.listen(port, async () => {
   console.log(`Listening on port ${port}.....`);
